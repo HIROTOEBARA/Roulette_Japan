8ec3a78a28838f3be481b8217a6f846d0d11486c
演習 <hriotowe112@icloud.com>
2024-12-16 23:21:40 +0900

    Initial Commit

---
+++ Roulette_Japan/Roulette_JapanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roulette_JapanApp.swift
+//  Roulette_Japan
+//
+//  Created by 江原弘人 on 2024/12/08.
+//
+
+import SwiftUI
+
+@main
+struct Roulette_JapanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RouletteView()
+        }
+    }
+}
---
+++ Roulette_JapanTests/Roulette_JapanTests.swift
@@ -0,0 +1,17 @@
+//
+//  Roulette_JapanTests.swift
+//  Roulette_JapanTests
+//
+//  Created by 江原弘人 on 2024/12/08.
+//
+
+import Testing
+@testable import Roulette_Japan
+
+struct Roulette_JapanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
